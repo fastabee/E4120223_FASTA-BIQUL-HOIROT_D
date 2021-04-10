12b4f0a0754d1c834adde83bf03fc4ae3619c605
E41202233_FASTA BIQUL HOIROT <80698899+fastabee@users.noreply.github.com>
2021-04-10 19:43:59 +0700

    Update graphApp.java

--- minggu 5/graphApp.java
+++ minggu 5/graphApp.java
@@ -7,15 +7,26 @@ package latihan;
 
 /**
  *
- * @author fastabee
+ * @author Khoirunnisa
  */
-public class Node {
-    public int id;
-    public String data;
-    public Node leftChild;
-    public Node rightChild;
-    
-    public void displayNode(){
-        System.out.print("{ " + id + ", " + data + " } ");
+public class GraphApp {
+    public static void main(String[] args){
+        
+        Graph theGraph = new Graph();
+        
+        theGraph.addVertex('A'); //0
+        theGraph.addVertex('B'); //1
+        theGraph.addVertex('C'); //2
+        theGraph.addVertex('D'); //3
+        theGraph.addVertex('E'); //4
+        
+        theGraph.addEdge(0, 1); //AB
+        theGraph.addEdge(1, 2); //BC
+        theGraph.addEdge(2, 3); //CD
+        theGraph.addEdge(3, 4); //DE
+        
+        theGraph.bfs();
+        
+//        theGraph.display();
     }
 }
